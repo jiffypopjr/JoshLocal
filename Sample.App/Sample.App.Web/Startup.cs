32fe9de3b9fce293706c180cd1e8f47952101f85
Joshua Smith <jsmith@learningmachine.com>
2014-04-08 17:32:44 -0500

    initial check -in

---
+++ Sample.App/Sample.App.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sample.App.Web.Startup))]
+namespace Sample.App.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
