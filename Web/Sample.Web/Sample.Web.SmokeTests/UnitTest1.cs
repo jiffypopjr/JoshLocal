4734b7685f1c28d19ca3e14e8013c37b32515461
Joshua Smith <jsmith@learningmachine.com>
2014-04-10 16:30:43 -0500

    added smoke test

---
+++ Web/Sample.Web/Sample.Web.SmokeTests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WatiN;
+using WatiN.Core;
+
+namespace Sample.Web.SmokeTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Verify_WebsiteUp()
+        {
+            using (var browser = new IE("http://localhost/Sample.Web_deploy"))
+            {
+                var hasText = browser.ContainsText("Getting started");
+                Assert.IsTrue(hasText);
+            }
+        }
+    }
+}
