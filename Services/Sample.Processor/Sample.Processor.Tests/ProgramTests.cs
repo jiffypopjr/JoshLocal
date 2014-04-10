8dbea022e40e3572face53bc92a3c35ac8390752
Joshua Smith <jsmith@learningmachine.com>
2014-04-10 12:12:44 -0500

    added tests

---
+++ Services/Sample.Processor/Sample.Processor.Tests/ProgramTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sample.Processor.Tests
+{
+    [TestClass]
+    public class PrograTests
+    {
+        [TestMethod]
+        public void Console_ShouldOpen()
+        {
+            
+        }
+    }
+}
