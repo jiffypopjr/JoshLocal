9836af14a1f961f4d3b256178ec9651ec8f1a8c0
Joshua Smith <jsmith@learningmachine.com>
2014-04-10 11:47:12 -0500

    initial

---
+++ Database/Sample.Db/Tables/UserAccounts.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[UserAccounts]
+(
+	[UserAccountId] INT NOT NULL identity(1,1) constraint PK_UserAccounts PRIMARY KEY,
+	[UserLoginEmail] varchar(100) not null,
+	[UserLoginPassword] varchar(50) not null,
+	constraint UC_UserAccounts_UserLoginEmail unique ([UserLoginEmail])
+)
